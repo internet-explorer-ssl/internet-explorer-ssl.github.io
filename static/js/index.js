4e27c1f15f30879955103d569adacebb7c7ac613
Ellis Brown <ellis.l.brown.ii@gmail.com>
2023-03-01 17:03:39 -0500

    bibtex highlighting

--- static/js/index.js
+++ static/js/index.js
@@ -99,3 +99,40 @@ $(document).mouseup(function (e) {
 });
 
 $(".method-overview-full-img").show()
+
+
+// bibtex copy
+// https://www.roboleary.net/2022/01/13/copy-code-to-clipboard-blog.html
+
+const copyButtonLabel = "copy";
+
+// use a class selector if available
+let blocks = document.querySelectorAll("pre");
+
+blocks.forEach((block) => {
+    // only add button if browser supports Clipboard API
+    if (navigator.clipboard) {
+        let button = document.createElement("button");
+
+        button.innerText = copyButtonLabel;
+        block.appendChild(button);
+
+        button.addEventListener("click", async () => {
+            await copyCode(block, button);
+        });
+    }
+});
+
+async function copyCode(block, button) {
+    let code = block.querySelector("code");
+    let text = code.innerText;
+
+    await navigator.clipboard.writeText(text);
+
+    // visual feedback that task is completed
+    button.innerText = "citation copied!";
+
+    setTimeout(() => {
+        button.innerText = copyButtonLabel;
+    }, 700);
+}
